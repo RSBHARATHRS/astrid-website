0b9d67cce04ff6f253b27ac82b75fe225ae3cf80
Bharath R <78311020+RSBHARATHRS@users.noreply.github.com>
2023-04-02 21:30:10 +0530

    website responsive issue

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+
