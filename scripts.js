609dcd79dd27b49611cdeda324d90db26a56d7e8
Bharath <bharath.r@idp.com>
2023-04-04 12:33:20 +0530

    some modifications done

--- scripts.js
+++ scripts.js
@@ -8,3 +8,12 @@ function closeNav() {
   document.getElementById("myOverlay").style.display = "none";
 }
 
+function openCity(cityName) {
+  var i;
+  var x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  document.getElementById(cityName).style.display = "block";  
+}
+
